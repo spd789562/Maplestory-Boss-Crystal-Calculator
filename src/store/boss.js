9d9223af1d1be5cfebc2cb92c1fd7489697cd794
spd789562 <leo.yicun.lin@gmail.com>
2020-11-18 17:13:50 +0800

    [add] toggle defeatable action

--- src/store/boss.js
+++ src/store/boss.js
@@ -1,7 +1,29 @@
 import { reducerCreator } from './_helper'
 
-const initialState = []
-const reducer = reducerCreator(initialState, {})
+import { curry, evolve, findIndex, pipe, propEq, not, update } from 'ramda'
+
+import BossMapping from '@mapping/bosses-crystal'
+
+export const TOGGLE_BOSS_DEEATABLE = 'TOGGLE_BOSS_DEEATABLE'
+
+const findBossIndexById = curry((id, bosses) =>
+  findIndex(propEq('id', id), bosses)
+)
+
+const initialState = BossMapping.map(({ id, difficulties }) => ({
+  id,
+  defeatable: true,
+  partyCount: 1,
+  defeatTime: 0,
+  difficulty: difficulties[difficulties.length - 1].difficulty,
+}))
+
+const reducer = reducerCreator(initialState, {
+  [TOGGLE_BOSS_DEEATABLE]: (state, payload) =>
+    pipe(findBossIndexById(payload), (index) =>
+      update(index, evolve({ defeatable: not }, state[index]), state)
+    )(state),
+})
 
 export default {
   initialState,
