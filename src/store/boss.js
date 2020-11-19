d9f2eaad03a567620f10eefb1ea8f54d21b1a668
spd789562 <leo.yicun.lin@gmail.com>
2020-11-19 17:40:01 +0800

    [add] boss data save to storage

--- src/store/boss.js
+++ src/store/boss.js
@@ -13,12 +13,20 @@ import {
 
 import BossMapping from '@mapping/bosses-crystal'
 
+export const INIT_BOSS_DATA = 'INIT_BOSS_DATA'
 export const TOGGLE_BOSS_DEFEATABLE = 'TOGGLE_BOSS_DEFEATABLE'
 export const UPDATE_BOSS_DATA = 'UPDATE_BOSS_DATA'
+
+const storageKey = 'MAPLESTORE_BOSS_CRYSTAL_CALCULATOR_DATA'
 
 const findBossIndexById = curry((id, bosses) =>
   findIndex(propEq('id', id), bosses)
 )
+
+const saveToStroage = (state) => {
+  localStorage.setItem(storageKey, JSON.stringify(state))
+  return state
+}
 
 const initialState = BossMapping.map(({ id, difficulties }) => ({
   id,
@@ -29,13 +37,19 @@ const initialState = BossMapping.map(({ id, difficulties }) => ({
 }))
 
 const reducer = reducerCreator(initialState, {
+  [INIT_BOSS_DATA]: (_, payload) => payload,
   [TOGGLE_BOSS_DEFEATABLE]: (state, payload) =>
-    pipe(findBossIndexById(payload), (index) =>
-      update(index, evolve({ defeatable: not }, state[index]), state)
+    pipe(
+      findBossIndexById(payload),
+      (index) =>
+        update(index, evolve({ defeatable: not }, state[index]), state),
+      saveToStroage
     )(state),
   [UPDATE_BOSS_DATA]: (state, payload) =>
-    pipe(findBossIndexById(payload.id), (index) =>
-      update(index, mergeRight(state[index], payload.data), state)
+    pipe(
+      findBossIndexById(payload.id),
+      (index) => update(index, mergeRight(state[index], payload.data), state),
+      saveToStroage
     )(state),
 })
 
