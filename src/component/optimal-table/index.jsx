57eaf0f69a04858a8a3daff41704b20bf115b9bb
spd789562 <leo.yicun.lin@gmail.com>
2020-11-25 18:08:43 +0800

    [add] optimal chart

--- src/component/optimal-table/index.jsx
+++ src/component/optimal-table/index.jsx
@@ -1,9 +1,11 @@
+import { Fragment, useRef, useState } from 'react'
+import dynamic from 'next/dynamic'
 /* store */
 import { useStore } from '@store'
 import { UPDATE_BOSS_DATA } from '@store/boss'
 
 /* components */
-import { Table, Avatar, Space } from 'antd'
+import { Table, Avatar, Space, Card, Row } from 'antd'
 
 /* i18n */
 import { withTranslation } from '@i18n'
@@ -16,6 +18,15 @@ import getBossSuggestion from '@utils/get-boss-suggestion'
 /* mapping */
 import BossMapping, { BossObject } from '@mapping/bosses-crystal'
 import MesosMapping from '@mapping/bosses-mesos'
+
+const Pie = dynamic(() => import('@ant-design/charts/lib/pie'), {
+  ssr: false,
+})
+
+const tabList = [
+  { key: 'table', tab: 'table' },
+  { key: 'chart', tab: 'chart' },
+]
 
 const columns = [
   {
@@ -53,35 +64,114 @@ const useTableData = (t) => {
   const totalMesos = mergedData.reduce((total, { mesos }) => total + mesos, 0)
   const totalCount = mergedData.reduce((total, { count }) => total + count, 0)
   return {
-    tableData: map(evolve({ mesos: numberFormat }), mergedData),
+    tableData: mergedData,
     totalCount,
     totalMesos: numberFormat(totalMesos),
   }
 }
 
+const Chart = ({ config }) => (
+  <div>{config.data.length && <Pie {...config} />}</div>
+)
+
 const OptimalTable = ({ t }) => {
+  const [tab, hangeChangeTab] = useState('table')
   const { tableData, totalCount, totalMesos } = useTableData(t)
+
+  const config = {
+    data: tableData,
+    angleField: 'mesos',
+    colorField: 'name',
+    appendPadding: 10,
+    radius: 1,
+    innerRadius: 0.6,
+    legend: false,
+    label: {
+      type: 'inner',
+      content: '{name}',
+      position: 'middle',
+      style: {
+        textAlign: 'center',
+        fontSize: 14,
+        fill: '#333',
+      },
+    },
+    pieStyle: {
+      lineWidth: 0,
+      lineDash: [0, 0],
+    },
+    state: {
+      active: {
+        style: {
+          lineWidth: 0,
+          fillOpacity: 0.65,
+        },
+      },
+    },
+    statistic: {
+      title: false,
+      content: {
+        style: {
+          whiteSpace: 'pre-wrap',
+          overflow: 'unset',
+          textOverflow: 'ellipsis',
+          fontSize: 18,
+        },
+        formatter: function formatter(args) {
+          return `<div style="display:flex;align-items:center;flex-direction:column;${
+            args ? 'transform:translateY(-50%)' : ''
+          }">
+            <img height="48px" src="${
+              args ? `/boss/${args.avatar}` : 'crystal'
+            }.png" alt=${args ? args.name : 'crystal'} />
+            <div>${args ? args.name : t('total')}</div>
+            <div>${t('count')}: ${args ? args.count : totalCount}</div>
+            <div>${t('mesos')}: ${numberFormat(
+            args ? args.mesos : totalMesos
+          )}</div>
+          </div>`
+        },
+      },
+    },
+    height: 500,
+    interactions: [
+      { type: 'element-active' },
+      { type: 'pie-statistic-active' },
+    ],
+  }
+
   return (
-    <Table
-      columns={columns.map(evolve({ title: t }))}
-      dataSource={tableData}
-      pagination={false}
-      rowKey="name"
-      title={() => t('optimal_result')}
-      summary={() => (
-        <Table.Summary.Row>
-          <Table.Summary.Cell>{t('total')}</Table.Summary.Cell>
-          <Table.Summary.Cell>
-            <div style={{ textAlign: 'center' }}>{totalCount}</div>
-          </Table.Summary.Cell>
-          <Table.Summary.Cell>
-            <div style={{ textAlign: 'right' }}>{totalMesos}</div>
-          </Table.Summary.Cell>
-        </Table.Summary.Row>
+    <Card
+      title={t('optimal_result')}
+      bodyStyle={{ padding: 0 }}
+      tabList={tabList}
+      activeTabKey={tab}
+      onTabChange={hangeChangeTab}
+    >
+      {tab === 'table' ? (
+        <Table
+          columns={columns.map(evolve({ title: t }))}
+          dataSource={map(evolve({ mesos: numberFormat }), tableData)}
+          pagination={false}
+          rowKey="name"
+          summary={() => (
+            <Table.Summary.Row>
+              <Table.Summary.Cell>{t('total')}</Table.Summary.Cell>
+              <Table.Summary.Cell>
+                <div style={{ textAlign: 'center' }}>{totalCount}</div>
+              </Table.Summary.Cell>
+              <Table.Summary.Cell>
+                <div style={{ textAlign: 'right' }}>{totalMesos}</div>
+              </Table.Summary.Cell>
+            </Table.Summary.Row>
+          )}
+          bordered
+          scroll={{ x: true }}
+        />
+      ) : (
+        <Chart config={config} />
       )}
-      bordered
-      scroll={{ x: true }}
-    />
+    </Card>
   )
 }
 
